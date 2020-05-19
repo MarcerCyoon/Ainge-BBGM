0c0ac48af861dcb2cba0a7398b9a78f2100bafa6
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2020-05-19 22:15:57 +0900

    Added auto-update functionality

--- ainge.py
+++ ainge.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import features
 
 def fileExists(fileName):
 	try:
@@ -26,14 +28,37 @@ while True:
 	print("U. Type U to Update Export with FAs")
 	print("T. Type T to Eliminate Transaction / Event")
 	print("H. Type H for Help")
-	choice = input().strip()
+	choice = input().strip().upper()
 
 	if (choice == "U"):
-		print("Woop")
+		csvName = input("Please tell us the Decision CSV's name (include .csv): ")
+
+		while True:
+			if (fileExists(csvName)):
+				print("Intializing export... \n")
+				with open(csvName.strip(), "r") as file:
+					reader = csv.reader(file)
+					decisionArr = []
+					i = 0
+
+					for row in reader:
+						decisionArr[i] = row
+						i += 1
+				break
+			else:
+				print("No such file exists!")
+				csvName = input("Please re-input the CSV's name (include .csv): ")
+
+		isResign = input("Is this concerning Re-signings? If yes, type 1. If not, type 0: ")
+
+		print("Beginning export update... \n")
+		features.updateExport(isResign, decisionArr, fileName)
 		break
+
 	elif (choice == "T"):
-		print("lol")
+		print("TBA")
 		break
+
 	elif (choice == "H"):
 		print("You can use Ainge-BBGM to update your export or eliminate a transaction at the current moment.\n")
 		print("Update Export: by typing U, you can choose to update your export with FA signings that were made.")
@@ -41,7 +66,9 @@ while True:
 		print("in the specified format and simply enter its name.")
 		print("The specified format is as follows: the columns must be Player Name, Team Signed With, Offer AAV, Offer Years")
 		print("in that order. Each player decision should be a row; keep in mind the program will skip row one, assuming")
-		print("it will be used for headers.\n")
-		print("Eliminate Transaction: by typing T, you can choose to eliminate an accidetanl transaction that occured")
+		print("that it will be used for headers.\n")
+		print("Eliminate Transaction: by typing T, you can choose to eliminate an accidetanl transaction that occurred,")
 		print("whether it be because of Ainge-BBGM or if you simply butterfingered a trade.")
 		print("All you must do is provide Ainge-BBGM with the information about the transaction.\n")
+
+input("Press ENTER to exit")
---
+++ features.py
@@ -0,0 +1,72 @@
+import json
+
+# updateExport() code copied entirely from DecisionMakerSLR.
+
+# Update an existing export named "export.json" with Free Agency decisions found in a decisionArr.
+# This should perfectly emulate actually signing them in the BBGM game.
+def updateExport(isResign, decisionArr, exportName):
+	print(decisionArr)
+
+	with open(exportName, "r", encoding='utf-8-sig') as file:
+	    export = json.load(file)
+
+	# Generate dictionary of each team and their tids
+	teamDict = dict()
+	for team in export['teams']:
+			teamName = team['region'] + " " + team['name']
+			teamDict[teamName] = team['tid']
+
+	text = export['meta']['phaseText']
+	currentYear = int(text.split(" ")[0])
+	phase = text.split(" ")[1]
+
+	for decision in decisionArr:
+		player = list(filter(lambda player: (player['firstName'].strip() + " " + player['lastName'].strip()) == decision[0], export['players']))[0]
+		tid = teamDict[decision[1]]
+		player['tid'] = tid
+		player['contract']['amount'] = decision[2] * 1000
+
+		# If the current phase of the game is the "Regular Season" or the "Preseason",
+		# then signing a contract will include the current year.
+		# For example, if Isaiah Thomas signs a 1-year deal in 2020, it will expire at the end of 2020.
+		# However, if you sign Isaiah Thomas in a 1-year deal in the 2020 offseason, it will expire at
+		# the end of 2021. Thus, a distinction needs to be made depending on the current phase.
+		if (phase == "regular" or phase == "preseason"):
+			exp = decision[3] + currentYear - 1
+			player['contract']['exp'] = exp
+		else:
+			exp = decision[3] + currentYear
+			player['contract']['exp'] = exp
+
+		# To fully emulate the way signings are worked in-game, we must also create a corresponding event
+		# in the game's code that tells us that so-and-so signed with such-and-such team. This is important
+		# not only for error-identification purposes, but also makes league history and transactions
+		# be kept complete.
+		event = dict()
+
+		# For some reason, the text for events only necessitates the team code and "label name": we only need
+		# Celtics, not Boston Celtics.
+		code = export['teams'][tid]['abbrev']
+		labelName = decision[1].split(" ")[-1]
+		
+		if (not isResign):
+			event['type'] = 'reSigned'
+			event['text'] = "The <a href=\"/l/1/roster/{}/{}\">{}</a> re-signed <a href=\"/l/1/player/{}\">{}</a> for ${}M/year through {}.".format(code, currentYear, labelName, player['pid'], decision[0], "%0.2f" % decision[2], exp)
+
+		else:
+			event['type'] = 'freeAgent'
+			event['text'] = "The <a href=\"/l/1/roster/{}/{}\">{}</a> signed <a href=\"/l/1/player/{}\">{}</a> for ${}M/year through {}.".format(code, currentYear, labelName, player['pid'], decision[0], "%0.2f" % decision[2], exp)
+		
+		event['pids'] = [player['pid']]
+		event['tids'] = [teamDict[decision[1]]]
+		event['season'] = currentYear
+		# The eid of the current event would have to be the next available eid.
+		# That would just be the current amount of events, as each event has a corresponding
+		# eid and they start at 0 instead of 1.
+		event['eid'] = len(export['events'])
+
+		export['events'].append(event)
+
+	with open("updated.json", "w") as file:
+		json.dump(export, file)
+		print("New Export Created.")
